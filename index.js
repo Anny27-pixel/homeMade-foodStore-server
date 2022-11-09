394bb282e867d0897bef8b91994c212b5f9d3074
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-09 20:51:13 +0600

    services database added

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pzlo6ar.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('homeMadeFood').collection('services');
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
 
 
 
